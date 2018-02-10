12fd304a409c59dd2236702019cdc254d2ec3b85
lihao <lihao3016@gmail.com>
2018-02-10 14:27:24 +0800

    issud bug in ch3:hough_lines

--- chapter3/hough_lines.py
+++ chapter3/hough_lines.py
@@ -7,7 +7,8 @@ edges = cv2.Canny(gray,50,120)
 minLineLength = 20
 maxLineGap = 5
 lines = cv2.HoughLinesP(edges,1,np.pi/180,20,minLineLength,maxLineGap)
-for x1,y1,x2,y2 in lines[0]:
+lines = lines[:,0,:]
+for x1,y1,x2,y2 in lines:
   cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
 cv2.imshow("edges", edges)
